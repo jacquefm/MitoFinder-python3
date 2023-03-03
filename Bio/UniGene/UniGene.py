38391d4b8d29929d08982409c043264fb10e8d29
jacquefm <74439498+jacquefm@users.noreply.github.com>
2023-03-03 12:53:29 -0500

    Add files via upload

--- Bio/UniGene/UniGene.py
+++ Bio/UniGene/UniGene.py
@@ -3,7 +3,7 @@
 # license.  Please see the LICENSE file that should have been included
 # as part of this package.
 
-from __future__ import print_function
+
 
 import warnings
 from Bio import BiopythonDeprecationWarning
