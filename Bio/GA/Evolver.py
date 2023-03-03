3771c59df12fa2bd914e8e52fd12de191f043382
jacquefm <74439498+jacquefm@users.noreply.github.com>
2023-03-03 12:16:11 -0500

    Add files via upload

--- Bio/GA/Evolver.py
+++ Bio/GA/Evolver.py
@@ -9,7 +9,7 @@ Evolver classes manage a population of individuals, and are responsible
 for taking care of the transition from one generation to the next.
 """
 # standard modules
-from __future__ import print_function
+
 
 import sys
 
